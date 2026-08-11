a3922280ac1494afc5735c7402c409c17d8be2ef
Aluno <e023aluno@outlook.com>
2026-08-11 20:19:43 -0300

    listar Produtos

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import FormProduto from "./components/FormProduto"
+
+export default function App(){
+
+
+  return(
+
+    <div style={{padding: 20} }>
+
+      <FormProduto/>
+    
+          
+    </div>
+  )
+}
